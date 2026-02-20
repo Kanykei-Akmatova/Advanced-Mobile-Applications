017f923e938f445bead557aa91b9b0c744858910
Kanykei-Akmatova <kanykeyakmatova@gmail.com>
2026-02-19 20:33:17 -0500

    Implementation of Lab 1-Login App

---
+++ Module1/app/src/main/java/com/example/module1/ui/theme/Theme.kt
@@ -0,0 +1,21 @@
+package com.example.loginapp.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFF1565C0),
+    secondary = Color(0xFF42A5F5),
+    background = Color(0xFFF5F5F5),
+    surface = Color.White
+)
+
+@Composable
+fun LoginAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        content = content
+    )
+}
